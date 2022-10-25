1000345fb38ef2390460d0d71c80818ba62ad018
iaretaruna <taruna.singh@jjay.cuny.edu>
2022-10-25 19:45:25 -0400

    Updated routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,20 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Button.js';
+import Component from "./Component";
+
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" index element={<>Hi</>}/>
+        <Route path="/Page2" element={<Component />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/Component.js
@@ -0,0 +1,5 @@
+const Component = (props) => {
+    return <div>hello</div>;
+  };
+
+  export default Component;
