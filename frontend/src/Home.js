a010c11966a257c57525c33412ca4efab321f426
Christopher Golden <jillian.golden@jjay.cuny.edu>
2022-11-08 18:27:39 -0500

    Home page and user

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <section>
+                <div class="container-fluid">
+                        <h1 class="mt-5">Welcome</h1>
+                </div>
+        </section>
+    );
+}
+
+export default Home;
