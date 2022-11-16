c76527421fe261a37d66c663e1a3670840a686c3
iaretaruna <taruna.singh@jjay.cuny.edu>
2022-11-15 20:05:58 -0500

    Updated page 2

--- frontend/src/Page2.js
+++ frontend/src/Page2.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./Page2.css";
 import { Button } from "bootstrap";
-<center></center>
+
 function Page2(){
     return(
         <section>
-                <div class="container-fluid">
-                        <h1 class="mt-5">Page 2</h1>
+                <div class="EnterNumber">
+                        <h1>Page 2</h1>
                         <input type="text" placeholder="Enter your phone number..."></input>
-                          
-                    
-                        <button>Enter</button>
+                             
+                </div>
+
+                <div class= "EnterButton">
+                <button>Enter</button>
                 </div>
         </section>
     );
