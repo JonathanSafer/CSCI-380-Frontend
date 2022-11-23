75e8a1ee0e5bfa118c6fa7a1ec8c725acd55ef7a
justin pile <jpile1999@gmail.com>
2022-11-22 20:02:06 -0500

    updated page3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import gif from "./Orate.gif"
 import Page2 from "./Page2"
 import User from "./User";
 import Home from "./Home";
+import Page3 from "./Page3";
 import Page4 from "./Page4";
 
 
@@ -15,11 +16,12 @@ return (
     
 
       <Routes>
-        <Route path="/" index element={<Home/> } />
-        <Route path='/page2' element={<Page2 />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path='/User' element={<User />} />
-        <Route path='/page4' element={<Page4 />} />
+        <Route path="/" index element={<>Hi </>}/>
+        <Route path= '/page2' element={<Page2/>}/>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path='/User' element={<User/>}/> 
+        <Route path='/page3' element={<Page3/>}/>
+        <Route path='/page4' element={<Page4/>}/> 
       </Routes>
       
     
