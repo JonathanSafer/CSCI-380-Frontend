48eaf79f2500d0228e5995b3a6e497c2eba0daf8
iaretaruna <taruna.singh@jjay.cuny.edu>
2022-11-25 16:55:47 -0500

    working on it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router-dom";
-//import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./orate.svg"
 import Page2 from "./Page2"
 import User from "./User";
 import Home from "./Home";
@@ -11,16 +11,34 @@ import Page4 from "./Page4";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>Ignore this Jonathan.. working on it</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Under Construction
+        </a>
+      </header>
+    
+    
       <Routes>
-        <Route path="/" index element={<>Hi </>}/>
+        <Route path="/" index element={<>Under constuction </>}/>
         <Route path= '/page2' element={<Page2/>}/>
         <Route path="/Home" element={<Home/>}/>
         <Route path='/User' element={<User/>}/> 
         <Route path='/page4' element={<Page4/>}/> 
       </Routes>
-    </>
+    </div>
+    
   );
+
 }
 
 export default App;
