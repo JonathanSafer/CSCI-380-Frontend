37c350f4eee69c8a6ad3528eb46f1023f3889de3
iaretaruna <taruna.singh@jjay.cuny.edu>
2022-11-27 23:17:17 -0500

    logo up

---
+++ frontend/src/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.css";
+import gif from "./Orate.gif"
+
+function Home(){
+return(
+    <section className = "orate-logo">
+        <div>
+            <img src = {gif} alt = "Logo" />
+            
+        </div>
+
+     
+        </section>
+    
+   
+
+
+
+
+
+
+
+
+)
+
+
+}
+
+export default Home;
