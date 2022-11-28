3a632b0145f2b422346b1618fc12dd2779750943
iaretaruna <taruna.singh@jjay.cuny.edu>
2022-11-27 23:17:17 -0500

    logo up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "./orate.svg"
+import gif from "./Orate.gif"
 import Page2 from "./Page2"
 import User from "./User";
 import Home from "./Home";
@@ -10,35 +10,23 @@ import Page4 from "./Page4";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>Ignore this Jonathan.. working on it</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Under Construction
-        </a>
-      </header>
+return (
+       <>
     
-    
-      <Routes>
-        <Route path="/" index element={<>Under constuction </>}/>
-        <Route path= '/page2' element={<Page2/>}/>
-        <Route path="/Home" element={<Home/>}/>
-        <Route path='/User' element={<User/>}/> 
-        <Route path='/page4' element={<Page4/>}/> 
-      </Routes>
-    </div>
-    
-  );
 
+      <Routes>
+        <Route path="/" index element={<Home/> } />
+        <Route path='/page2' element={<Page2 />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path='/User' element={<User />} />
+        <Route path='/page4' element={<Page4 />} />
+      </Routes>
+      
+    
+</>
+
+   
+)
 }
 
 export default App;
