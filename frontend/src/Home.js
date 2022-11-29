9440b7dddc00e3520d58022236b6f03a565d50d5
Christopher Golden <jillian.golden@jjay.cuny.edu>
2022-11-29 16:12:02 -0500

    bio/pic

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,45 +1,20 @@
-import React from 'react';;
-import styled from 'styled-components';
+import React from 'react';
+import "./Home.css";
+import { Button } from "bootstrap";
 
-const theme={
-red:{
-    default:'#ff0000',
-    hover:'#ffcccb'
-}
-}
-const Button = styled.button'
-background-color: ${props=>theme[props.theme].default};
-color:white;
-padding: 5px 15px;
-border-radius:5px;
-outline:0;
-text-transform: uppercase;
-cursor:Pointer; 
-box-shadow:0px 2px 2px lightgray;
-transition: ease back-ground color 250ms;
-&:hover{
-    background-color: ${props=>theme[props.theme].hover};
+function Home(){
 
-}
-'
+    const handleClick = () => {
+        console.log('hello');
+    }
 
-Button.defaultProps={
-    theme:'red'
-}
-function clickMe(){
-    alert('You clicked me!');
-}
-
-export default function App(){
     return(
-        <div>
-            <button>
-                CLICK HERE
-            </button>
-        </div>
+        <section>
+                <div class="Home">
+                    <h1> Homepage</h1>
+                    <button onClick={handleClick}>Click Me!</button>
+                </div>
+        </section>
     );
-    
-}
-   
 
-        
+}export default Home;
