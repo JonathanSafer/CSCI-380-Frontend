dbbc077ec1858b0db50baa20ac3aed9a4de340f0
Jonathan Safer <jonathansafer2@gmail.com>
2022-11-29 18:59:14 -0500

    fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,20 +16,11 @@ return (
     
 
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" index element={<>Hi </>}/>
+        <Route path="/" index element={<Home/>}/>
         <Route path= '/page2' element={<Page2/>}/>
-        <Route path="/Home" element={<Home/>}/>
         <Route path='/User' element={<User/>}/> 
         <Route path='/page3' element={<Page3/>}/>
         <Route path='/page4' element={<Page4/>}/> 
-=======
-        <Route path="/" index element={<Home/> } />
-        <Route path='/page2' element={<Page2 />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path='/User' element={<User />} />
-        <Route path='/page4' element={<Page4 />} />
->>>>>>> 3db1b8e475ea44fac8a11d2611120d9010bc89c5
       </Routes>
       
     
