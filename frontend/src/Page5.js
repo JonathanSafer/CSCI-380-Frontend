128803d8df55fdb74d1b169ca744f1370a24a29f
Andy1094 <113483733+Andy1094@users.noreply.github.com>
2022-12-06 19:31:09 -0500

    merged

---
+++ frontend/src/Page5.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Page5.css";
+
+function Page5(){
+    return(
+        <section>
+                <div class="container-fluid">
+                        <h1 class="mt-5">Page 4</h1>
+                        <input type="text" placeholder="Search Contact..."></input>
+
+                        {/* all of these buttons should direct to the phone book page */}
+                        <button class="button1">Call</button>
+                        <button type="button">Video Call</button>
+                        <button type="button">Message</button>
+                </div>
+        </section>
+
+
+    );
+    
+}
+export default Page5;
